d6afb60bdb64a2687e22047e790f50ed024c49a1
Valentin Mikleu <valentin.mikleu@documenta.ro>
2023-08-10 10:41:14 +0300

    MAI-1044 Added Swagger and Sl4j

--- src/main/java/ro/infrasoft/ws/JustWsClientApplication.java
+++ src/main/java/ro/infrasoft/ws/JustWsClientApplication.java
@@ -1,13 +1,30 @@
 package ro.infrasoft.ws;
 
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class JustWsClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JustWsClientApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+		return new OpenAPI()
+				.info(new Info()
+								.title("INTEGR-JUST-RO")
+								.version(appVersion)
+								.description(appDesciption)
+								.termsOfService("http://swagger.io/terms/"));
 	}
 
 }
--- src/main/java/ro/infrasoft/ws/SoapClinet.java
+++ src/main/java/ro/infrasoft/ws/SoapClinet.java
@@ -1,5 +1,7 @@
 package ro.infrasoft.ws;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Service;
 import org.springframework.ws.client.core.WebServiceTemplate;
@@ -7,15 +9,18 @@ import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
 import ro.infrasoft.ws.dto.*;
 
 import javax.xml.soap.MessageFactory;
-
 @Service
+@Slf4j
 public class SoapClinet {
 
     public static final String DTO_PACKAGE = "ro.infrasoft.ws.dto";
     public static final String JUST_RO_URL = "http://portalquery.just.ro/query.asmx";
 
+    public static final ObjectMapper objectMapper =  new ObjectMapper();
     public CautareDosare2Response cautareDosare2(CautareDosare2 cautareDosare2) {
         try {
+            String jsonCautareDosare2 = objectMapper.writeValueAsString(cautareDosare2);
+            log.info("S-a cautat:" + "\n" + jsonCautareDosare2);
             SaajSoapMessageFactory messageFactory = new SaajSoapMessageFactory(
                     MessageFactory.newInstance());
             messageFactory.afterPropertiesSet();
@@ -33,16 +38,19 @@ public class SoapClinet {
                     .marshalSendAndReceive(
                             JUST_RO_URL,
                             cautareDosare2);
+            String jsonCautareDosare2Response = objectMapper.writeValueAsString(cautareDosare2Response);
+            log.info("Rezultat: " + "\n" + jsonCautareDosare2Response);
             return cautareDosare2Response;
         } catch (Exception s) {
-            s.printStackTrace();
-            System.out.println(s);
+            log.error(String.valueOf(s));
         }
         return null;
     }
 
     public CautareSedinteResponse cautareSedinte(CautareSedinte cautareSedinte) {
         try {
+            String jsonCautareSedinte = objectMapper.writeValueAsString(cautareSedinte);
+            log.info("S-a cautat:" + "\n" + jsonCautareSedinte);
             SaajSoapMessageFactory messageFactory = new SaajSoapMessageFactory(
                     MessageFactory.newInstance());
             messageFactory.afterPropertiesSet();
@@ -63,10 +71,11 @@ public class SoapClinet {
                             JUST_RO_URL,
                             cautareSedinte);
 
+            String jsonCautareSedinteResponse = objectMapper.writeValueAsString(cautareSedinteResponse);
+            log.info("Rezultat: " + "\n" + jsonCautareSedinteResponse);
             return cautareSedinteResponse;
         } catch (Exception s) {
-            s.printStackTrace();
-            System.out.println(s);
+            log.error(String.valueOf(s));
         }
         return null;
     }
